e950353d8ece3c10149113b47faa068f977ebf0f
Danny <dannyolan@Dannys-MBP.attlocal.net>
2020-09-16 15:02:22 -0500

    This is project one base

--- sketch.js
+++ sketch.js
@@ -1,9 +1,54 @@
+let r, g, b;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-  background(255,200,10);
-  // put drawing code here
+  background(250);
+
+
+
+
+  strokeWeight(150);
+  stroke(r, g, b);
+  rectMode(CORNER);
+  fill(r,g,b);
+  rect(mouseX,mouseY,50,50);
+}
+
+function mousePressed() {
+  let d = dist (windowWidth,windowHeight,windowWidth,windowHeight);
+  if (d < 100) {
+
+    r = random(255);
+    g = random(255);
+    b = random(255);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
